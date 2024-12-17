9d751dad8c9a95b089af8055f2b88ea11d0fd3ef
Miguel <miguelm@hotmail.com.ar>
2024-12-17 14:01:15 +0000

    Created custom logic to get new ID

--- srv/event-service.js
+++ srv/event-service.js
@@ -1,11 +1,24 @@
-
-
-
 module.exports = cds.service.impl(async function() {
-  const bpService = await cds.connect.to('API_BUSINESS_PARTNER');
+    const { 
+        Events, 
+        Participants, 
+        BusinessPartners 
+    } = this.entities;
 
-  // Explicitly handle READ requests for BusinessPartners
-  this.on('READ', 'BusinessPartners', async (req) => {
-      return bpService.run(req.query); // Pass the query to the external service
-  });
+    /*
+     * Custom logic to get a new ID before creating a record in the Events or Participants table.
+     * The function `getNextId` is called to determine the next available ID for the respective table.
+     */
+    
+    this.before('CREATE', 'Events', async (req) => {
+        const tableName = req.target.name; 
+        const newID = await getNextId(tableName); // Get the next available ID for the Events table
+        req.data.ID = newID;
+    });
+    
+    this.before('CREATE', 'Participants', async (req) => {
+        const tableName = req.target.name;
+        const newID = await getNextId(tableName); // Get the next available ID for the Participants table
+        req.data.ID = newID;
+    });
 });
